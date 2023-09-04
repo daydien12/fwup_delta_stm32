732fe6c9dda785520afe887323184c5d71f04d05
daydien12 <31497738+daydien12@users.noreply.github.com>
2023-09-04 23:26:37 +0700

    update

--- 3.tool/2.uart/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/c/Users/nhanma/Documents/github/fwup_delta_stm32/3.tool/2.uart
-# Build directory: /mnt/c/Users/nhanma/Documents/github/fwup_delta_stm32/3.tool/2.uart
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
